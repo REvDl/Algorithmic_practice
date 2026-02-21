97a4399548cded28df915f26e8e094d8f0bfaeb2
REvDi <peresadok33@gmail.com>
2026-02-21 15:03:07 +0200

    Sliding window

--- exercise/2026/easy/longest_substring.py
+++ exercise/2026/easy/longest_substring.py
@@ -13,3 +13,19 @@ def longest_substring(s):
 				answer = right - left + 1
 	return answer
 
+
+
+def longest_window(s):
+	answer, left, right = 0, 0, 0
+	char_set = set()
+	while right < len(s):
+		c = s[right]
+		if c not in char_set:
+			char_set.add(c)
+			answer = max(answer, right - left + 1)
+			right += 1
+		else:
+			while c in char_set:
+				char_set.remove(s[left])
+				left += 1
+	return answer

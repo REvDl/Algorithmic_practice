197a1edb7bdb02458b20acd7b7b5296649ab6d09
REvDi <peresadok33@gmail.com>
2026-02-21 18:25:43 +0200

    exercise

--- exercise/2026/easy/TwoSum.py
+++ exercise/2026/easy/TwoSum.py
@@ -8,26 +8,3 @@ class Solution(object):
 			else:
 				result[num] = i
 		return None
-
-
-
-
-
-
-
-
-
-def longest_window(s):
-	answer, left, right = 0, 0, 0
-	char_set = set()
-	while right < len(s):
-		c = s[right]
-		if c not in char_set:
-			char_set.add(c)
-			answer = max(answer, right - left + 1)
-			right += 1
-		else:
-			while c in char_set:
-				char_set.remove(s[left])
-				left += 1
-	return answer

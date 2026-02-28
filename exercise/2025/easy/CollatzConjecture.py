13d23d60503fd79a196f385fccd3b504825af361
REvDi <peresadok33@gmail.com>
2026-02-28 23:29:47 +0200

    Exercise 2025 Collatz

---
+++ exercise/2025/easy/CollatzConjecture.py
@@ -0,0 +1,11 @@
+def steps(number):
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    steps = 0
+    while number != 1:
+        steps +=1
+        if number % 2 == 0:
+             number = number // 2
+        elif number % 2 != 0:
+                number = (number * 3) + 1
+    return steps

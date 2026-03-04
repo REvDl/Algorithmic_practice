597082b8623695707e6cdddf881f20c128a59880
REvDi <peresadok33@gmail.com>
2026-03-05 00:32:53 +0200

    Leetcode rob

---
+++ leetcode/2026/medium/rob.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+	def rob(self, nums: List[int]) -> int:
+		if not nums:
+			return 0
+		prev1 = 0
+		prev2 = 0
+		for num in nums:
+			tmp = prev1
+			prev1 = max(prev2 + num, prev1)
+			prev2 = tmp
+		return prev1
+
+obj = Solution()
+print(obj.rob([3,1,1,3]))

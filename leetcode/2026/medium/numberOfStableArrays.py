357d1dc36a94d6ad6b0c6222413cfb2e5fc1747f
REvDi <peresadok33@gmail.com>
2026-03-11 00:48:46 +0200

    Leetcode numberOfStableArrays

---
+++ leetcode/2026/medium/numberOfStableArrays.py
@@ -0,0 +1,24 @@
+class Solution:
+	def numberOfStableArrays(self, zero: int, one: int, limit: int):
+		"""нужно вернуть кол-во стабильных масивов, масив считается стабильным если выполняется 3 условия:
+			1. кол-во 0 == zero
+			2. кол-во 1 == one
+			3. Максимальная длина подряд идущих одинаковых элементов ≤ limit
+		"""
+		MOD = 10 ** 9 + 7
+		dp = [[[0,0] for _ in range(one+1)] for _ in range(zero+1)]
+		for i in range(1, min(zero, limit)+1):
+			dp[i][0][0] = 1
+		for j in range(1, min(one, limit)+1):
+			dp[0][j][1] = 1
+		for i in range(1, zero+1):
+			for j in range(1, one + 1):
+				over0 = dp[i - limit - 1][j][1] if i-limit >= 1 else 0
+				over1 = dp[i][j - limit - 1][0] if j-limit >= 1 else 0
+
+				dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1] - over0) % MOD
+				dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1] - over1) % MOD
+		return (dp[zero][one][0] + dp[zero][one][1]) % MOD
+
+obj = Solution()
+print(obj.numberOfStableArrays(4, 6, 10))

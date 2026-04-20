cb8064da569c060431626c15fd74872e868cd1fb
REvDi <peresadok33@gmail.com>
2026-04-20 10:59:11 +0300

    Leetcode 2078 maxDistance(daily task)

--- leetcode/2026/easy/maxDistance.py
+++ leetcode/2026/easy/maxDistance.py
@@ -13,12 +13,21 @@ class Solution:
 				result = max(result, (abs(j - (n - 1))))
 		return result
 
-
-
+	def maxDistance_v2(self, colors: List[int]) -> int:
+		max_diss = 0
+		for i in range(len(colors)-1, -1, -1):
+			if colors[i] != colors[0]:
+				max_diss = max(max_diss, i)
+				break
+		for j in range(len(colors)-1):
+			if colors[j] != colors[-1]:
+				max_diss = max(max_diss, abs(j - (len(colors) - 1)))
+				break
+		return max_diss
 
 obj = Solution()
-colors = [0,1]
+colors = [4,4,4,11,4,4,11,4,4,4,4,4]
 print(obj.maxDistance(colors))
-
+print(obj.maxDistance_v2(colors))
 
 

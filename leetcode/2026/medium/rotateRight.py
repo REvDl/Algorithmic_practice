a81e97f8c0820e951dea0c7abbafefa16d110e8e
REvDi <peresadok33@gmail.com>
2026-05-05 14:55:52 +0300

    Leetcode 61 rotateRight (dirty code)

---
+++ leetcode/2026/medium/rotateRight.py
@@ -0,0 +1,37 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+	def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+		if not head or k == 0: return head
+		curr = head
+		len_head = 1
+		while curr.next:
+			len_head += 1
+			curr = curr.next
+		k = k % len_head
+		if k == 0:
+			return head
+		curr.next = head
+		slice = len_head - k
+		i = 1
+		node = head
+		start_node = None
+		while node.next:
+			if i == slice:
+				start_node = node.next
+				node.next = None
+				break
+			i += 1
+			node = node.next
+		return start_node
+
+obj = Solution()
+head = [1,2,3,4,5]
+k = 2
+print(obj.rotateRight(head, k))

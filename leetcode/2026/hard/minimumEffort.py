f41739e289d0f4186d18dc37e89ef98dd403907d
REvDi <peresadok33@gmail.com>
2026-05-12 23:57:37 +0300

    Leetcode 1665 minimumEffort

---
+++ leetcode/2026/hard/minimumEffort.py
@@ -0,0 +1,23 @@
+from typing import List
+from operator import itemgetter
+
+class Solution:
+	def minimumEffort(self, tasks: List[List[int]]) -> int:
+		tasks.sort(key=lambda x: x[0] - x[1])
+		energy = 0
+		current = 0
+		for task in tasks:
+			diff = task[1] - energy
+			while energy < task[1]:
+				energy += diff
+				current += diff
+			energy -= task[0]
+		return current
+
+
+
+obj = Solution()
+tasks = [[1,7],[2,8],[3,9],[4,10],[5,11],[6,12]]
+print(obj.minimumEffort(tasks))
+
+

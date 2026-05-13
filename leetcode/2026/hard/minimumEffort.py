0f442c7c53de7913a00a53efb5a58edc5b84e99b
REvDi <peresadok33@gmail.com>
2026-05-13 20:04:17 +0300

    Leetcode 1665 rewrite for revision

--- leetcode/2026/hard/minimumEffort.py
+++ leetcode/2026/hard/minimumEffort.py
@@ -15,8 +15,50 @@ class Solution:
 
 
 
+
+
+
+
+
+
+
+
+	def minimumEffort_v2(self, tasks: List[List[int]]) -> int:
+		tasks.sort(key=lambda x: x[0] - x[1])
+		energy = 0
+		current = 0
+		for task in tasks:
+			diff = task[1] - energy
+			if energy < task[1]:
+				energy += diff
+				current += diff
+			energy -= task[0]
+
+		return current
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 obj = Solution()
 tasks = [[1,7],[2,8],[3,9],[4,10],[5,11],[6,12]]
 print(obj.minimumEffort(tasks))
-
+print(obj.minimumEffort_v2(tasks))
 

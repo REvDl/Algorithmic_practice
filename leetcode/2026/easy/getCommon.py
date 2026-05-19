75ed69f597b938e6d5a98a0ec57105e8ea98ac0d
REvDi <peresadok33@gmail.com>
2026-05-19 15:48:47 +0300

    Leetcode 2540 getCommon (two pointers)

--- leetcode/2026/easy/getCommon.py
+++ leetcode/2026/easy/getCommon.py
@@ -2,13 +2,25 @@ from typing import List
 
 
 class Solution:
-    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+    def getCommon_set(self, nums1: List[int], nums2: List[int]) -> int:
         nums2 = set(nums2)
         for i in range(len(nums1)):
             if nums1[i] in nums2:
                 return nums1[i]
         else:
             return -1
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        one_pointer, two_pointer = 0, 0
+        while one_pointer < len(nums1) and two_pointer< len(nums2):
+            if nums1[one_pointer] > nums2[two_pointer]:
+                two_pointer += 1
+            elif nums1[one_pointer] < nums2[two_pointer]:
+                one_pointer += 1
+            else:
+                return nums1[one_pointer]
+        return -1
+
+
 
 
 

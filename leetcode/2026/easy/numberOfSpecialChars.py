2935f5d6a4537b132a3784451eea8303d2e2c4d5
REvDi <peresadok33@gmail.com>
2026-05-26 14:06:20 +0300

    Leetcode 3120 numberOfSpecialChars (intersection of sets)

--- leetcode/2026/easy/numberOfSpecialChars.py
+++ leetcode/2026/easy/numberOfSpecialChars.py
@@ -7,7 +7,7 @@
 
 
 class Solution:
-    def numberOfSpecialChars(self, word: str) -> int:
+    def numberOfSpecialChars_v1(self, word: str) -> int:
         lower_set = set()
         upper_set = set()
         result = 0
@@ -21,6 +21,24 @@ class Solution:
             if char.upper() in upper_set:
                 result += 1
         return result
+
+
+
+    def numberOfSpecialChars(self, word: str) -> int:
+        lower_set = set()
+        upper_set = set()
+        for char in word:
+            if char.isupper():
+                upper_set.add(char)
+            else:
+                lower_set.add(char)
+        lower_in_upper = {char.upper() for char in lower_set}
+        common_chars = lower_in_upper & upper_set
+        return len(common_chars)
+
+
+
+
 
 obj = Solution()
 word = "aaAbcBC"

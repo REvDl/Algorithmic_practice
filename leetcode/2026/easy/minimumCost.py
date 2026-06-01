54a81d81f231b551d6d24ef59aed201ebb58dd30
REvDi <peresadok33@gmail.com>
2026-06-01 17:49:35 +0300

    Leetcode 2144 minimumCost

---
+++ leetcode/2026/easy/minimumCost.py
@@ -0,0 +1,25 @@
+from typing import List
+from itertools import zip_longest
+
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        total_cost = 0
+        cost.sort(reverse=True)
+        for i in range(0, len(cost), 3):
+            data = cost[i:i+3]
+            if len(data) == 3:
+                total_cost += data[0] + data[1]
+            else:
+                total_cost += sum(data) 
+        return total_cost
+
+    def minimumCost_v2(self, cost: List[int]) -> int:
+        total_cost = 0
+        cost.sort(reverse=True)
+        for i in range(0, len(cost), 3):
+            total_cost += sum(cost[i:i+2])
+        return total_cost
+
+obj = Solution()
+cost = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
+print(obj.minimumCost(cost))

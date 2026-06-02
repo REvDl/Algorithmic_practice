7ca4c45888dfbec7ac69e304a3ce73fb6e7da9d3
REvDi <peresadok33@gmail.com>
2026-06-02 19:03:13 +0300

    Leetcode 3635 earliestFinishTime

---
+++ leetcode/2026/medium/earliestFinishTime.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+
+        minLand = min(landStartTime[i] + landDuration[i] for i in range(len(landStartTime)))
+        minWater = min(waterStartTime[i] + waterDuration[i] for i in range(len(waterStartTime)))
+
+        finish_water = float("inf")
+        finish_land = float("inf")
+
+        for i in range(len(waterStartTime)):
+            start_water = max(minLand, waterStartTime[i])
+            finish_water = min(start_water + waterDuration[i], finish_water)
+
+        for i in range(len(landStartTime)):
+            start_land = max(minWater, landStartTime[i])
+            finish_land = min(start_land + landDuration[i], finish_land)
+
+        return min(finish_land, finish_water)
+

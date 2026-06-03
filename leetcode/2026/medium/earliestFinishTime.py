e0298efb2a89026f8e17c0e49201fed947b49358
REvDi <peresadok33@gmail.com>
2026-06-03 15:03:49 +0300

    Leetcode 3635 earliestFinishTime (repetition and new solution DRY)

--- leetcode/2026/medium/earliestFinishTime.py
+++ leetcode/2026/medium/earliestFinishTime.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class Solution:
-    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+    def earliestFinishTime_yesterday(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
 
         minLand = min(landStartTime[i] + landDuration[i] for i in range(len(landStartTime)))
         minWater = min(waterStartTime[i] + waterDuration[i] for i in range(len(waterStartTime)))
@@ -19,20 +19,23 @@ class Solution:
 
         return min(finish_land, finish_water)
 
-    def earliestFinishTime_2(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int],
-                             waterDuration: List[int]) -> int:
-        n, m = len(landStartTime), len(waterStartTime)
-        minLand = min(landStartTime[i] + landDuration[i] for i in range(n))
-        minWater = min(waterStartTime[i] + waterDuration[i] for i in range(m))
 
-        finish_land = float("inf")
-        finish_water = float("inf")
+    def get_earliest_end(self, StartTime: List[int], Duration: List[int]) -> int:
+        return min(StartTime[i] + Duration[i] for i in range(len(StartTime)))
 
-        for i in range(n):
-            start_land = max(minWater, landStartTime[i])
-            finish_land = min(start_land + landDuration[i], finish_land)
+    def solve_one_way(self, FirstStartTime: List[int], FirstDuration: List[int], SecondStartTime: List[int],
+                             SecondDuration: List[int]) -> int:
+        min_end = self.get_earliest_end(FirstStartTime, FirstDuration)
+        earliest_end = float("inf")
+        for i, start_time in enumerate(SecondStartTime):
+            start = min_end if min_end > start_time else start_time
+            current_finish = start + SecondDuration[i]
+            if current_finish < earliest_end:
+                earliest_end = current_finish
+        return earliest_end
 
-        for j in range(m):
-            start_water = max(minLand, waterStartTime[j])
-            finish_water = min(start_water + waterDuration[j], finish_water)
-        return min(finish_water, finish_land)
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int],
+                           waterDuration: List[int]) -> int:
+        return min(self.solve_one_way(landStartTime, landDuration, waterStartTime, waterDuration),
+                   self.solve_one_way(waterStartTime, waterDuration, landStartTime, landDuration)
+        )

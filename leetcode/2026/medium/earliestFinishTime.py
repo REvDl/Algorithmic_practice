4ce4e0bebe74de3c0c2f9688703b5c9c47e6d706
REvDi <peresadok33@gmail.com>
2026-06-03 13:46:40 +0300

    Leetcode 3635 earliestFiishTime (repetition)

--- leetcode/2026/medium/earliestFinishTime.py
+++ leetcode/2026/medium/earliestFinishTime.py
@@ -19,3 +19,20 @@ class Solution:
 
         return min(finish_land, finish_water)
 
+    def earliestFinishTime_2(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int],
+                             waterDuration: List[int]) -> int:
+        n, m = len(landStartTime), len(waterStartTime)
+        minLand = min(landStartTime[i] + landDuration[i] for i in range(n))
+        minWater = min(waterStartTime[i] + waterDuration[i] for i in range(m))
+
+        finish_land = float("inf")
+        finish_water = float("inf")
+
+        for i in range(n):
+            start_land = max(minWater, landStartTime[i])
+            finish_land = min(start_land + landDuration[i], finish_land)
+
+        for j in range(m):
+            start_water = max(minLand, waterStartTime[j])
+            finish_water = min(start_water + waterDuration[j], finish_water)
+        return min(finish_water, finish_land)

490f14460446457d93151257ed5917a7ce11a843
REvDi <peresadok33@gmail.com>
2026-06-28 18:10:49 +0300

    Leetcode 1846 maximumELementAfterDecrementingAndRearranging

---
+++ leetcode/2026/medium/maximumElementAfterDecrementingAndRearranging.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            arr[i] = min(arr[i-1] + 1, arr[i])
+        return arr[-1]
+
+
+
+obj = Solution()
+arr = [100, 1, 1000]
+print(obj.maximumElementAfterDecrementingAndRearranging(arr))
+

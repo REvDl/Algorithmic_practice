5aa42a639a7f09b3604dbe9c87d50664aa63e3a3
REvDi <peresadok33@gmail.com>
2026-07-19 19:18:56 +0300

    Leetcode 1081 smallestSubsequence

---
+++ leetcode/2026/medium/smallestSubsequence.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        stack = []
+        count_char = Counter(s)
+        visited = set()
+        for char in s:
+            count_char[char] -= 1
+            if char in visited:
+                continue
+            visited.add(char)
+            while stack and stack[-1] > char and count_char[stack[-1]] > 0:
+                visited.remove(stack[-1])
+                stack.pop()
+            stack.append(char)
+        return "".join(stack)
+
+
+
+
+
+obj = Solution()
+s = "cbacdcbc"
+print(obj.smallestSubsequence(s))

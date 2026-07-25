e110e5ee40c17de10a74b1bd831a87ee8cc47c27
REvDi <peresadok33@gmail.com>
2026-07-25 13:50:09 +0300

    Leetcode 3536 maxProduct
    feat(maxProduct): Implement optimized maxProduct using sorting

--- leetcode/2026/easy/maxProduct.py
+++ leetcode/2026/easy/maxProduct.py
@@ -2,15 +2,18 @@ import heapq
 
 
 class Solution:
-    def maxProduct(self, n: int) -> int:
+    def maxProduct_v1(self, n: int) -> int:
         nums = [int(x) for x in str(n)]
         two_max = heapq.nlargest(2, nums)
         return two_max[0] * two_max[1]
 
-
+    def maxProduct(self, n: int) -> int:
+        nums = sorted(str(n))
+        return int(nums[-1]) * int(nums[-2])
 
 
 obj = Solution()
 n = 124
 res = obj.maxProduct(n)
 print(res)
+assert res == 8

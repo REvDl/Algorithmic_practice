b152e3960ffb8cf116cd85a9a53229dbeec17011
REvDl <peresadok33@gmail.com>
2026-08-09 21:15:53 +0300

    feat: add new param for player
    write this by Phone in journey

--- My_fantasy/Acode_try.py
+++ My_fantasy/Acode_try.py
@@ -3,8 +3,10 @@
 
 
 class Player:
-  def __init__(self, hp: int):
+  def __init__(self, name: str | None = "Jane doe", hp: int | None = 100, icon: str | None = "*"):
     self.hp = hp
+    self.name = name
+    self.icon = icon
     
   
   
@@ -14,8 +16,8 @@ class Player:
 def input_settings():
   try:
     hp_val = int(input("Input your hp: " ))
-    player = Player(hp_val)
-    return f"Your hp: {player.hp}"
+    player = Player(hp=hp_val)
+    return f"Your player: {player.hp, player.name, player.icon}"
   except Exception as e:
     return f"Error {e}, pleace try latter."
     

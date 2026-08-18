9b62616148dd04e80b9c3ebde67fed0270326d4c
REvDl <peresadok33@gmail.com>
2026-08-18 22:19:51 +0300

    Leetcode 268 missingNumber

---
+++ leetcode/2026/easy/missingNumber.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+      n = len(nums)
+      need_sum = 0
+      for num in range(n+1):
+        need_sum += num
+      return need_sum - sum(nums)
+      
+
+
+
+obj = Solution()
+nums = [0, 1]
+print(obj.missingNumber(nums))

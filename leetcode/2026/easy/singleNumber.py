22f3c9e10b950a0b3069945efe279ced9ff0de2e
REvDl <peresadok33@gmail.com>
2026-08-18 22:41:06 +0300

    Leetcode 136 singleNumber

---
+++ leetcode/2026/easy/singleNumber.py
@@ -0,0 +1,13 @@
+from pickle import BINUNICODE
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+      res = 0
+      for num in nums: res ^= num
+      return res
+
+
+obj = Solution()
+nums 

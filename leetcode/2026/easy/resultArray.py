f0617d828f966805569ab4a6fd6d391d46177b8a
REvDi <peresadok33@gmail.com>
2026-08-20 10:36:09 +0300

    Leetcode 3069 resultArray

---
+++ leetcode/2026/easy/resultArray.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        arr1, arr2 = [nums[0]], [nums[1]]
+        for i in range(2, n):
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
+        return arr1 + arr2
+
+
+
+obj = Solution()
+nums = [5,4,3,8]
+print(obj.resultArray(nums))

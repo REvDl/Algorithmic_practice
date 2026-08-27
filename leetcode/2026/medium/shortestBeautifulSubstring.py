fa3d4707a6f59887af31e521bc1fd2b2b2425625
REvDi <peresadok33@gmail.com>
2026-08-27 23:43:33 +0300

    Leetcode 2904 shortestBeautifulSubstring

--- leetcode/2026/medium/shortestBeautifulSubstring.py
+++ leetcode/2026/medium/shortestBeautifulSubstring.py
@@ -2,7 +2,7 @@
 
 
 class Solution:
-    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+    def shortestBeautifulSubstring_V1(self, s: str, k: int) -> str:
         n = len(s)
         substrings = []
         left = 0
@@ -13,6 +13,21 @@ class Solution:
                 if substring.count("1") == k:
                     substrings.append(substring)
         return "" if not substrings else min(substrings, key=lambda x: (len(x), x))
+
+
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        n, left, count = len(s), 0, 0
+        res = ""
+        for right in range(n):
+            if s[right] == "1":
+                count += 1
+            while count == k:
+                current = s[left : right + 1]
+                res = current if not res else min(res, current, key=lambda x: (len(x), x))
+                if s[left] == "1":
+                    count -= 1
+                left += 1
+        return res
 
 
 

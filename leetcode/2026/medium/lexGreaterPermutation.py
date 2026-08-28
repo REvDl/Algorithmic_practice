c38fc51b27319478faf3a86445e3468e74d33777
REvDi <peresadok33@gmail.com>
2026-08-28 22:57:21 +0300

    Leetcode 3720 lexGreaterPermutation

--- leetcode/2026/medium/lexGreaterPermutation.py
+++ leetcode/2026/medium/lexGreaterPermutation.py
@@ -1,17 +1,37 @@
-import itertools
+from collections import Counter
 
 
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
-        strings = []
-        for p in itertools.permutations(s):
-            res = "".join(p)
-            if res > target:
-                strings.append(res)
-        return "" if not strings else min(strings)
+        count = Counter(s)
+        greater_sring = []
+        n = len(s)
+        for i in range(n):
+            char = target[i]
+            if count[char] > 0:
+                greater_sring.append(char)
+                count[char] -= 1
+                res = "".join(count.elements())
+                sorted_target = "".join(sorted(res, reverse=True))
+                if sorted_target > target[i+1:] and i <= n:
+                    continue
+                greater_sring.pop()
+                count[char] += 1 
+            available_chars = sorted(count.keys())
+            for sorted_char in available_chars:
+                if sorted_char > char and count[sorted_char] > 0:
+                    greater_sring.append(sorted_char)
+                    count[sorted_char] -= 1
+                    remaining = "".join(sorted(count.elements()))
+                    return "".join(greater_sring) + remaining
+            return ""
+        return ""
+                
+
+
 
 
 obj = Solution()
-s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
-target = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+s = "abc"
+target = "bba"
 print(obj.lexGreaterPermutation(s, target))

6f6274fb5c0398ac22e521d10f35c2473a78b9a6
REvDi <peresadok33@gmail.com>
2026-09-02 02:40:50 +0300

    Leetcode 3568 minMovesClassroom

--- leetcode/2026/medium/minMovesClassroom.py
+++ leetcode/2026/medium/minMovesClassroom.py
@@ -5,30 +5,41 @@ from collections import deque
 
 
 class Solution:
-    def _bfs(self, rows:int, cols:int, matrix, x:int, y:int, energy:int, let:int, total_let:int, steps:int):
+    def _bfs(self, rows:int, cols:int, matrix, x:int, y:int, energy:int, let:int, total_let:int, l_garbage_ids):
         dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        visited = {(x,y)}
-        q = deque([(x,y)])
+        visited = {(x, y, energy, let)}
+        q = deque([(x, y, energy, let, 0)]) 
         max_e = energy
         while q:
-            r, c = q.popleft()
-
+            r, c, energy, let, steps = q.popleft()
+            if let == (1 << total_let) - 1:
+                return steps
+            elif energy == 0 and matrix[r][c] != "R":
+                continue
             for dr, dc in dirs:
                 nr, nc = r+dr, c+dc
-                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visited and matrix[nr][nc] != "X":
-                    steps += 1
-                    energy -= 1
-                    if energy == 0 and matrix[nr][nc] != "R":
-                        print("break")
-                        break
+                if 0 <= nr < rows and 0 <= nc < cols and matrix[nr][nc] != "X":
+                    if matrix[nr][nc] == "R":
+                        if (nr, nc, max_e, let) not in visited:
+                            visited.add((nr, nc, max_e, let))
+                            q.append((nr, nc, max_e, let, steps+1))
+                        continue
+                        
                     elif matrix[nr][nc] == "L":
-                        let += 1
-                    elif matrix[nr][nc] == "R":
-                        print("RR")
-                        energy = max_e
-                    visited.add((nr, nc))
-                    q.append((nr, nc))
-        return steps
+                        garbage_id = l_garbage_ids[(nr, nc)]
+                        new_mask = let | (1 << garbage_id)
+                        if (nr, nc, energy-1, new_mask) not in visited:
+                            visited.add((nr, nc, energy-1, new_mask))
+                            q.append((nr, nc, energy-1, new_mask, steps+1))
+                        continue
+                        
+                    else:
+                        if (nr, nc, energy-1, let) not in visited:
+                            visited.add((nr, nc, energy-1, let))
+                            q.append((nr, nc, energy-1, let, steps+1))
+                        continue
+
+        return -1
     def minMoves(self, classroom: List[str], energy: int) -> int:
         """
         S - start position
@@ -40,20 +51,25 @@ class Solution:
         n = len(classroom)
         matrix = [list(row) for row in classroom]
         rows, cols = len(matrix), len(matrix[0])
+        l_garbage_ids = {} 
+        l_counter = 0
         total_let = 0
         S = None
-        for i, row in enumerate(matrix):
-            total_let += row.count("L")
-            if not S and "S" in row:
-                j = row.index("S")
-                S = (i, j)
-                continue
-        return self._bfs(rows, cols, matrix, S[0], S[1], energy, 0, total_let, 0)
-
+        for r in range(rows):
+            for c in range(cols):
+                cell = matrix[r][c]
+                if cell == "L":
+                    l_garbage_ids[(r, c)] = l_counter
+                    l_counter += 1
+                elif cell == "S":
+                    S = (r, c)
+        total_let = l_counter
+        initial_mask  = 0
+        return self._bfs(rows, cols, matrix, S[0], S[1], energy, initial_mask, total_let, l_garbage_ids)
 
 
 
 obj = Solution()
-classroom =["S.", "XL"]
-energy = 2
+classroom = ["L.", "R.", "LL", "S.", "L.", "XR", ".."]
+energy = 7
 print(obj.minMoves(classroom, energy))

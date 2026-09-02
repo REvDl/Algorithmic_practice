25683299de25d4b0183b866d85042de658eb77c2
REvDi <peresadok33@gmail.com>
2026-09-03 02:46:16 +0300

    Leetcode 3875 uniformArray

--- leetcode/2026/easy/uniformArray.py
+++ leetcode/2026/easy/uniformArray.py
@@ -2,20 +2,25 @@ from typing import List
 
 
 class Solution:
-    def uniformArray_v1(self, nums1: list[int]) -> bool:
-        len_even, len_odd = 0, 0
+    def uniformArray_honest(self, nums1: list[int]) -> bool:
         n = len(nums1)
+        can_even_total = 0
+        can_odd_total = 0
         for i in range(n):
-            if nums1[i] % 2 == 0:
-                len_even += 1
-            else:
-                len_odd += 1
-            for j in range(i+1,n):
-                if (nums1[i] - nums1[j]) % 2 == 0:
-                    len_even += 1
-                else:
-                    len_odd += 1
-        return True if len_odd >= n or len_even >= n else False
+            found_even = False
+            found_odd = False
+            for j in range(n):
+                if i == j:
+                    continue
+                if nums1[i] % 2 == 0 or (nums1[i] - nums1[j]) % 2 == 0:
+                    found_even = True
+                if nums1[i] % 2 != 0 or (nums1[i] - nums1[j]) % 2 != 0:
+                    found_odd = True
+                if found_even and found_odd:
+                    break
+            if found_even: can_even_total += 1
+            if found_odd:  can_odd_total += 1
+        return can_even_total == n or can_odd_total == n
 
 
     def uniformArray_v2(self, nums1: list[int]) -> bool:

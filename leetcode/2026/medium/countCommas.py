4b71dff6f0c4cda26053d02602761481b7cc6562
REvDi <peresadok33@gmail.com>
2026-09-09 23:53:43 +0300

    Leetcode 3871 countCommas (refactor)

--- leetcode/2026/medium/countCommas.py
+++ leetcode/2026/medium/countCommas.py
@@ -1,13 +1,25 @@
 
 
 class Solution:
-    def countCommas(self, n: int) -> int:
+    def countCommas_v1(self, n: int) -> int:
         one_comma = max(n - 999, 0)
         two_comma = max(n - 999_999, 0)
         three_comma = max(n - 999_999_999, 0)
         fourth_comma = max(n - 999_999_999_999, 0)
         five_comma = max(n - 999_999_999_999_999, 0)
         return one_comma + two_comma + three_comma + fourth_comma + five_comma
+
+
+    def countCommas(self, n: int) -> int:
+        diff_numbers = [
+            999, 
+            999_999, 
+            999_999_999,
+            999_999_999_999, 
+            999_999_999_999_999, 
+            999_999_999_999_999_999
+                        ]
+        return sum([max(n - num_for_diff, 0) for num_for_diff in diff_numbers]
 
 
 obj = Solution()

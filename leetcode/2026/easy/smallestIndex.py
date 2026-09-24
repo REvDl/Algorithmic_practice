9ff3817774b632ba2784314dd1357ebcccb41164
REvDi <peresadok33@gmail.com>
2026-09-24 16:39:34 +0300

    Leetcode 3550 smallestIndex

---
+++ leetcode/2026/easy/smallestIndex.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def _sum_num(self, num: int) -> int:
+        res = 0
+        while num > 0:
+            res += num % 10
+            num //= 10
+        return res
+
+
+    def smallestIndex(self, nums: List[int]) -> int:
+        ans = float('inf')
+        for idx, num in enumerate(nums):
+            res = self._sum_num(num)
+            if res == idx:
+                ans = min(ans, idx)
+        return ans if ans != float('inf') else -1
+
+obj = Solution()
+nums = [1,11,11]
+print(obj.smallestIndex(nums))
